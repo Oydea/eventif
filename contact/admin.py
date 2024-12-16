318c0995de6e0839f2f308f7f1810081f50874af
Oydea <luiz.silva@aluno.riogrande.ifrs.edu.br>
2024-12-16 13:47:41 -0300

    retirar campo inutil search_fields

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from contact.models import Contact
 class ContactModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'phone', 'message', 'created_at', 'reply_check', 'response', 'reply_created_at']
     date_hierarchy = 'created_at'
-    search_fields = ('name', 'email', 'phone', 'cpf', 'message','created_at')
+    search_fields = ('name', 'email', 'phone', 'message','created_at')
     list_filter = ('created_at', 'reply_check')
 
 admin.site.register(Contact, ContactModelAdmin)
